f5227572572af5cc937b1730f05f6e85ea630b80
TimRiebesam <42938378+TimRiebesam@users.noreply.github.com>
2020-05-07 13:36:26 +0200

    Restaurant Controller Error-Meldung hinzugefügt.

--- src/main/java/gastrome/api/services/RestaurantServiceImpl.java
+++ src/main/java/gastrome/api/services/RestaurantServiceImpl.java
@@ -1,7 +1,10 @@
 package gastrome.api.services;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.UUID;
+
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,13 +20,22 @@ public class RestaurantServiceImpl implements RestaurantService{
 	RestaurantRepository restaurantRepository;
 	
 	@Override
-	public List<Restaurant> getAllRestaurants() {
-		return restaurantRepository.findAll();
+	public List<Restaurant> getAllRestaurants(HttpServletResponse response) throws IOException {
+		List<Restaurant> restaurants = restaurantRepository.findAll();
+		if(!restaurants.isEmpty())
+			return restaurants;
+		
+		response.sendError(404, "Keine Restaurants vorhanden!");
+		return null;
 	}
 
 	@Override
-	public Restaurant getRestaurant(UUID restaurantId) {
-		return restaurantRepository.findById(restaurantId).orElse(null);
+	public Restaurant getRestaurant(UUID restaurantId, HttpServletResponse response) throws IOException {
+		Restaurant restaurant = restaurantRepository.findById(restaurantId).orElse(null);
+		if(restaurant == null)
+			response.sendError(404, "Restaurant nicht gefunden!");
+		
+		return restaurant;
 	}
 	
 }
